b59d2535e33972020bce88997ca3aaedbd2e876b
alexponi <alexponi@mail.ru>
2016-06-19 02:19:42 +0500

    Add Card

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160616211741) do
+ActiveRecord::Schema.define(version: 20160618204617) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
